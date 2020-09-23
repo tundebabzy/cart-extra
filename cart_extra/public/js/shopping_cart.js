61b7a23142353ea983ded0bca811b539e3e94324
Tunde Akinyanmi <tundebabzy@gmail.com>
2020-09-23 15:49:53 +0100

    - extend erpnext.shopping_cart   - `set_cart_count` and `update_cart` no longer try to log user   - sets guest specific cookie (ec_sid) when the website loads

--- cart_extra/public/js/shopping_cart.js
+++ cart_extra/public/js/shopping_cart.js
@@ -1,4 +1,39 @@
+const shopping_cart = erpnext.shopping_cart;
+
+frappe.ready(function() {
+    frappe.call({
+        method: 'cart_extra.utils.create_cart'
+    });
+});
+
 erpnext.shopping_cart = $.extend(erpnext.shopping_cart, {
+    set_cart_count: function() {
+		var cart_count = frappe.get_cookie("cart_count");
+
+		if(cart_count) {
+			$(".shopping-cart").toggleClass('hidden', false);
+		}
+
+		var $cart = $('.cart-icon');
+		var $badge = $cart.find("#cart-count");
+
+		if(parseInt(cart_count) === 0 || cart_count === undefined) {
+			$cart.css("display", "none");
+			$(".cart-items").html('Cart is Empty');
+			$(".cart-tax-items").hide();
+			$(".btn-place-order").hide();
+			$(".cart-addresses").hide();
+		}
+		else {
+			$cart.css("display", "inline");
+		}
+
+		if(cart_count) {
+			$badge.html(cart_count);
+		} else {
+			$badge.remove();
+		}
+	},
     update_cart: function(opts) {
         return frappe.call({
             type: "POST",
