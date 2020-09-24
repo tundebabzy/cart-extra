bdc5059999be912ac7fcd690d846d8204b81e4b5
Tunde Akinyanmi <tundebabzy@gmail.com>
2020-09-24 20:11:34 +0100

    clean up

--- cart_extra/templates/pages/cart.py
+++ cart_extra/templates/pages/cart.py
@@ -4,7 +4,5 @@ no_cache = 1
 no_sitemap = 1
 
 
-print('---------------------------8888------------------')
 def get_context(context):
-    print('overriden the context')
     context.update(get_cart_quotation())
