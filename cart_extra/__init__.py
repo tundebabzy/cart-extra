235b9ed0f13cc334e8011647da243ee07127e819
Tunde Akinyanmi <tundebabzy@gmail.com>
2020-09-26 18:44:56 +0100

    monkey patch erpnext..cart.get_context

--- cart_extra/__init__.py
+++ cart_extra/__init__.py
@@ -5,9 +5,24 @@ from __future__ import unicode_literals
 import frappe
 from frappe.website import router
 from frappe.website.router import get_start_folders, get_page_info
+from erpnext.templates.pages import cart
+from cart_extra.templates.pages.cart import get_context
 import os
 
 __version__ = '0.0.1'
+
+original_context_fn = cart.get_context
+
+
+def get_context_fn(context):
+    """
+    This monkey-patch lets us dynamically determine use the cart-extra defined
+    `get_context` function or default to the original
+    """
+    if frappe.session.user == 'Guest':
+        return get_context(context)
+    else:
+        return original_context_fn(context)
 
 
 def get_page_info_from_template(path):
@@ -35,3 +50,4 @@ def get_page_info_from_template(path):
 
 
 router.get_page_info_from_template = get_page_info_from_template
+cart.get_context = get_context_fn
